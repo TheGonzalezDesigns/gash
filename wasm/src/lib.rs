c8425806710cd20b7e32bf0723464a179e10a1d3
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-02 05:59:07 -0700

    Expose PathFinder struct, find path in wasm using Rust

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,10 +3,21 @@ mod rooms;
 mod pathfinding;
 
 pub use rooms::Room;
+pub use pathfinding::PathFinder;  // Expose the PathFinder struct
 
 use wasm_bindgen::prelude::*;
+use serde::{Serialize, Deserialize};
+use serde_wasm_bindgen;
 
 #[wasm_bindgen]
 pub fn greet() {
     web_sys::console::log_1(&"Hello from Rust!".into());
+}
+
+#[wasm_bindgen]
+pub fn wasm_find_path(start_room: usize, room_grid_js: &JsValue) -> Option<Vec<usize>> {
+    let grid: rooms::RoomGrid = room_grid_js.into_serde().unwrap();
+    let mut pathfinder = pathfinding::PathFinder::new();
+    let path = pathfinder.find_path(rooms::RoomId(start_room), &grid);
+    path.map(|p| p.into_iter().map(|room_id| room_id.0).collect())
 }
