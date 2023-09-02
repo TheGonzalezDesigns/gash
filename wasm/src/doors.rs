84c40307e7edd119275c4069efa5dd19b1ff67a3
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-02 01:28:32 -0700

    This commit introduces tests for door accessibility and creation.  - Add tests for door accessibility from inside and outside - Add a test for door creation  (20 tokens)

--- wasm/src/doors.rs
+++ wasm/src/doors.rs
@@ -43,3 +43,53 @@ impl Door {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_door_accessibility_from_inside() {
+        let door1 = Door::new(DoorLock::LockedFromInside);
+        assert_eq!(door1.is_accessible_from_inside(), false);
+
+        let door2 = Door::new(DoorLock::LockedFromOutside);
+        assert_eq!(door2.is_accessible_from_inside(), false);
+
+        let door3 = Door::new(DoorLock::UnlockedFromInside);
+        assert_eq!(door3.is_accessible_from_inside(), true);
+
+        let door4 = Door::new(DoorLock::UnlockedFromOutside);
+        assert_eq!(door4.is_accessible_from_inside(), true);
+    }
+
+    #[test]
+    fn test_door_accessibility_from_outside() {
+        let door1 = Door::new(DoorLock::LockedFromInside);
+        assert_eq!(door1.is_accessible_from_outside(), false);
+
+        let door2 = Door::new(DoorLock::LockedFromOutside);
+        assert_eq!(door2.is_accessible_from_outside(), false);
+
+        let door3 = Door::new(DoorLock::UnlockedFromInside);
+        assert_eq!(door3.is_accessible_from_outside(), true);
+
+        let door4 = Door::new(DoorLock::UnlockedFromOutside);
+        assert_eq!(door4.is_accessible_from_outside(), true);
+    }
+
+    #[test]
+    fn test_door_creation() {
+        let states = [
+            DoorLock::LockedFromInside,
+            DoorLock::LockedFromOutside,
+            DoorLock::UnlockedFromInside,
+            DoorLock::UnlockedFromOutside,
+        ];
+
+        for &state in states.iter() {
+            let door = Door::new(state);
+            assert_eq!(door.lock, state);
+        }
+    }
+}
