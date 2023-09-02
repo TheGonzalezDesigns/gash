816cd213dff38b4ae2320434a3a204d445b1be26
TheGonzalezDesigns <hugo@thegonzalez.design>
2023-09-02 04:44:38 -0700

    Add path-finding functionality and room highlighting functionality to the application

--- www/index.ts
+++ www/index.ts
@@ -1,13 +1,19 @@
-import init, { greet } from "./production/wasm.js";
-import { animate } from "./modules/three";
+import init, { greet, find_path } from "./production/wasm.js";
+import { animate, highlightRooms } from "./modules/three";
 
 async function runWasm() {
   await init();
   greet();
+
+  // Assuming the Rust function's name is 'find_path' and it returns an array of room indices
+  const path = find_path();
+
+  if (path) {
+    highlightRooms(path);
+  }
 }
 
 runWasm();
-
 animate();
 
 console.log("Hello via Bun!");
